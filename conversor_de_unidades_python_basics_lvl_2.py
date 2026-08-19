1c2b96bd0c1afb24562ab209a31db8e62000a8e6
KalelA23 <kamg2311@outlook.com>
2026-08-19 12:10:47 -0600

    Conversor de monedas python (Atlas proyect)
    Hi! i if you detect any problem you can tell me with out problem and ill solve it faster as i can

---
+++ conversor_de_unidades_python_basics_lvl_2.py
@@ -0,0 +1,187 @@
+Moneda = input("Seleccione la moneda que quiere convertir (USD, EUR, JPY, GBP, CNY, CHF): ")
+conversor = input("Seleccione a qué moneda quiere convertirla (USD, EUR, JPY, GBP, CNY, CHF): ")
+
+if Moneda == "USD":
+    if conversor == "EUR":
+        cantidad = float(input("Seleccione la cantidad de dolares que quiere convertir a euros: "))
+        resultado = cantidad * 0.86
+        print("Usted tiene:", resultado, "Euros")
+
+if Moneda == "USD":
+    if conversor == "JPY":
+        cantidad = float(input("Seleccione la cantidad de dolares que quiere convertir a yenes: "))
+        resultado = cantidad * 158.43
+        print("Usted tiene:", resultado, "Yenes")
+
+if Moneda == "USD":
+    if conversor == "GBP":
+        cantidad = float(input("Seleccione la cantidad de dolares que quiere convertir a libras esterlinas: "))
+        resultado = cantidad * 0.73
+        print("Usted tiene:", resultado, "Libras esterlinas")
+
+if Moneda == "USD":
+    if conversor == "CNY":
+        cantidad = float(input("Seleccione la cantidad de dolares que quiere convertir a yuanes: "))
+        resultado = cantidad * 6.73
+        print("Usted tiene:", resultado, "Yuanes")
+
+if Moneda == "USD":
+    if conversor == "CHF":
+        cantidad = float(input("Seleccione la cantidad de dolares que quiere convertir a francos suizos: "))
+        resultado = cantidad * 0.80
+        print("Usted tiene:", resultado, "Francos suizos")
+
+
+if Moneda == "EUR":
+    if conversor == "USD":
+        cantidad = float(input("Seleccione la cantidad de euros que quiere convertir a dolares: "))
+        resultado = cantidad * 1.17
+        print("Usted tiene:", resultado, "Dolares")
+
+if Moneda == "EUR":
+    if conversor == "JPY":
+        cantidad = float(input("Seleccione la cantidad de euros que quiere convertir a yenes: "))
+        resultado = cantidad * 184.84
+        print("Usted tiene:", resultado, "Yenes")
+
+if Moneda == "EUR":
+    if conversor == "GBP":
+        cantidad = float(input("Seleccione la cantidad de euros que quiere convertir a libras esterlinas: "))
+        resultado = cantidad * 0.86
+        print("Usted tiene:", resultado, "Libras esterlinas")
+
+if Moneda == "EUR":
+    if conversor == "CNY":
+        cantidad = float(input("Seleccione la cantidad de euros que quiere convertir a yuanes: "))
+        resultado = cantidad * 7.85
+        print("Usted tiene:", resultado, "Yuanes")
+
+if Moneda == "EUR":
+    if conversor == "CHF":
+        cantidad = float(input("Seleccione la cantidad de euros que quiere convertir a francos suizos: "))
+        resultado = cantidad * 0.93
+        print("Usted tiene:", resultado, "Francos suizos")
+
+
+if Moneda == "JPY":
+    if conversor == "USD":
+        cantidad = float(input("Seleccione la cantidad de yenes que quiere convertir a dolares: "))
+        resultado = cantidad * 0.0063
+        print("Usted tiene:", resultado, "Dolares")
+
+if Moneda == "JPY":
+    if conversor == "EUR":
+        cantidad = float(input("Seleccione la cantidad de yenes que quiere convertir a euros: "))
+        resultado = cantidad * 0.0054
+        print("Usted tiene:", resultado, "Euros")
+
+if Moneda == "JPY":
+    if conversor == "GBP":
+        cantidad = float(input("Seleccione la cantidad de yenes que quiere convertir a libras esterlinas: "))
+        resultado = cantidad * 0.0046
+        print("Usted tiene:", resultado, "Libras esterlinas")
+
+if Moneda == "JPY":
+    if conversor == "CNY":
+        cantidad = float(input("Seleccione la cantidad de yenes que quiere convertir a yuanes: "))
+        resultado = cantidad * 0.042
+        print("Usted tiene:", resultado, "Yuanes")
+
+if Moneda == "JPY":
+    if conversor == "CHF":
+        cantidad = float(input("Seleccione la cantidad de yenes que quiere convertir a francos suizos: "))
+        resultado = cantidad * 0.0051
+        print("Usted tiene:", resultado, "Francos suizos")
+
+
+if Moneda == "GBP":
+    if conversor == "USD":
+        cantidad = float(input("Seleccione la cantidad de libras esterlinas que quiere convertir a dolares: "))
+        resultado = cantidad * 1.37
+        print("Usted tiene:", resultado, "Dolares")
+
+if Moneda == "GBP":
+    if conversor == "EUR":
+        cantidad = float(input("Seleccione la cantidad de libras esterlinas que quiere convertir a euros: "))
+        resultado = cantidad * 1.16
+        print("Usted tiene:", resultado, "Euros")
+
+if Moneda == "GBP":
+    if conversor == "JPY":
+        cantidad = float(input("Seleccione la cantidad de libras esterlinas que quiere convertir a yenes: "))
+        resultado = cantidad * 216.90
+        print("Usted tiene:", resultado, "Yenes")
+
+if Moneda == "GBP":
+    if conversor == "CNY":
+        cantidad = float(input("Seleccione la cantidad de libras esterlinas que quiere convertir a yuanes: "))
+        resultado = cantidad * 9.24
+        print("Usted tiene:", resultado, "Yuanes")
+
+if Moneda == "GBP":
+    if conversor == "CHF":
+        cantidad = float(input("Seleccione la cantidad de libras esterlinas que quiere convertir a francos suizos: "))
+        resultado = cantidad * 1.07
+        print("Usted tiene:", resultado, "Francos suizos")
+
+
+if Moneda == "CNY":
+    if conversor == "USD":
+        cantidad = float(input("Seleccione la cantidad de yuanes que quiere convertir a dolares: "))
+        resultado = cantidad * 0.15
+        print("Usted tiene:", resultado, "Dolares")
+
+if Moneda == "CNY":
+    if conversor == "EUR":
+        cantidad = float(input("Seleccione la cantidad de yuanes que quiere convertir a euros: "))
+        resultado = cantidad * 0.13
+        print("Usted tiene:", resultado, "Euros")
+
+if Moneda == "CNY":
+    if conversor == "JPY":
+        cantidad = float(input("Seleccione la cantidad de yuanes que quiere convertir a yenes: "))
+        resultado = cantidad * 23.56
+        print("Usted tiene:", resultado, "Yenes")
+
+if Moneda == "CNY":
+    if conversor == "GBP":
+        cantidad = float(input("Seleccione la cantidad de yuanes que quiere convertir a libras esterlinas: "))
+        resultado = cantidad * 0.11
+        print("Usted tiene:", resultado, "Libras esterlinas")
+
+if Moneda == "CNY":
+    if conversor == "CHF":
+        cantidad = float(input("Seleccione la cantidad de yuanes que quiere convertir a francos suizos: "))
+        resultado = cantidad * 0.12
+        print("Usted tiene:", resultado, "Francos suizos")
+
+
+if Moneda == "CHF":
+    if conversor == "USD":
+        cantidad = float(input("Seleccione la cantidad de francos suizos que quiere convertir a dolares: "))
+        resultado = cantidad * 1.25
+        print("Usted tiene:", resultado, "Dolares")
+
+if Moneda == "CHF":
+    if conversor == "EUR":
+        cantidad = float(input("Seleccione la cantidad de francos suizos que quiere convertir a euros: "))
+        resultado = cantidad * 1.07
+        print("Usted tiene:", resultado, "Euros")
+
+if Moneda == "CHF":
+    if conversor == "JPY":
+        cantidad = float(input("Seleccione la cantidad de francos suizos que quiere convertir a yenes: "))
+        resultado = cantidad * 198.06
+        print("Usted tiene:", resultado, "Yenes")
+
+if Moneda == "CHF":
+    if conversor == "GBP":
+        cantidad = float(input("Seleccione la cantidad de francos suizos que quiere convertir a libras esterlinas: "))
+        resultado = cantidad * 0.92
+        print("Usted tiene:", resultado, "Libras esterlinas")
+
+if Moneda == "CHF":
+    if conversor == "CNY":
+        cantidad = float(input("Seleccione la cantidad de francos suizos que quiere convertir a yuanes: "))
+        resultado = cantidad * 8.41
+        print("Usted tiene:", resultado, "Yuanes")
