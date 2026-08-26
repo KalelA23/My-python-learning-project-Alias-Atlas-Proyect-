64d3ed91b234e59237b845f5e286b0aab3b32cae
KalelA23 <kamg2311@outlook.com>
2026-08-26 09:05:20 -0600

    Dice game!
    The program will show up 100 random numbers and at the end it will tell you how many appeared each number
    python basic lvl

---
+++ juego_dado_python_basic_lvl_1.py
@@ -0,0 +1,43 @@
+import random
+
+uno = 0
+dos = 0
+tres = 0
+cuatro = 0
+cinco = 0
+seis = 0
+
+for i in range(100):
+    resultado = random.randint(1,6)
+
+    if resultado == 1:
+        uno += 1
+
+    elif resultado == 2:
+        dos += 1
+
+    elif resultado == 3:
+        tres += 1
+
+    elif resultado == 4:
+        cuatro += 1
+
+    elif resultado == 5:
+        cinco += 1
+
+    elif resultado == 6:
+        seis += 1
+
+print("Las veces que aparecieron cada numero fueron:")
+
+print("1:", uno)
+
+print("2:", dos)
+
+print("3:", tres)
+
+print("4:", cuatro)
+
+print("5:", cinco)
+
+print("6:", seis)
