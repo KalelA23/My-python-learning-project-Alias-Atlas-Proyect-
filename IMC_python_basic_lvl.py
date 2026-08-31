0496dd206559d55c7f7a065543147aafb58b5567
KalelA23 <kamg2311@outlook.com>
2026-08-31 09:26:15 -0600

    IMC program
    IMC program made by me python basic lvl

---
+++ IMC_python_basic_lvl.py
@@ -0,0 +1,5 @@
+I = float(input("Indique su estatura en metros:       "))
+M = int(input("Indique su peso en kilogramos:         "))
+C = I ** 2
+IMC = M / C
+print("Su indice de masa corporal es de:",IMC)
